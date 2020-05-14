2704284a962d1d3b0a3cb60a9a856f008b1f5dc5
roybhaskar9 <bhaskar@study9.com>
2020-05-14 22:23:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "resourceGroup1"
+  location = "West US"
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "acceptanceTestPublicIp1"
+  location            = "West US"
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+data "template_file" "vmip" {
+  template = "${file("${vmip.tpl")}"
+  vars = {
+    vm_ip = "${azurerm_public_ip.example.ip_address}"
+  }
+}
